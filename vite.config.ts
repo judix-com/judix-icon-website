bca8f8bb91fd14805d7b455266f9f5c74d066661
shivamsaksham <shivamsaksham2000@gmail.com>
2026-01-12 22:52:54 +0530

    route updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoBase = process.env.GITHUB_PAGES === 'true' ? '/icon-pack-site/' : '/'
+const repoBase = process.env.GITHUB_PAGES === 'true' ? '/judix-icon-website/' : '/'
 
 // https://vite.dev/config/
 export default defineConfig({
